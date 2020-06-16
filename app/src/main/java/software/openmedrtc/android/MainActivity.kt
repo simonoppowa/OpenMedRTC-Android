5bc16e698e119019aee91a225cb883c8fb294b9f
Simon Oppowa <simon.oppowa@st.oth-regensburg.de>
2020-06-16 13:43:55 +0200

    added patient main fragment

--- app/src/main/java/software/openmedrtc/android/MainActivity.kt
+++ app/src/main/java/software/openmedrtc/android/MainActivity.kt
@@ -1,11 +1,16 @@
 package software.openmedrtc.android
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import software.openmedrtc.android.core.PatientMainFragment
 import software.openmedrtc.android.core.platform.BaseActivity
 
 class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openFragment(PatientMainFragment(), R.id.main_fragment_container)
     }
 }
--- app/src/main/java/software/openmedrtc/android/core/platform/BaseActivity.kt
+++ app/src/main/java/software/openmedrtc/android/core/platform/BaseActivity.kt
@@ -1,6 +1,15 @@
 package software.openmedrtc.android.core.platform
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 open class BaseActivity: AppCompatActivity() {
+
+    open fun openFragment(fragment: Fragment, containerId: Int) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(containerId, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 }
