be2bbd89546da30227fa696e89bfff515b5a9e78
Simon Oppowa <simon.oppowa@st.oth-regensburg.de>
2020-06-20 14:42:10 +0200

    added message types

--- app/src/main/java/software/openmedrtc/android/features/shared/connection/DataMessage.kt
+++ app/src/main/java/software/openmedrtc/android/features/shared/connection/DataMessage.kt
@@ -1,7 +1,15 @@
 package software.openmedrtc.android.features.shared.connection
 
-data class DataMessage(val messageType: String , val json: String) {
+data class DataMessage(val messageType: String, val json: String) {
     companion object {
         const val MESSAGE_TYPE_PATIENTS_LIST = "PATIENTS_LIST"
     }
 }
+
+data class SdpMessage(
+    val fromUser: String,
+    val toUser: String,
+    val sessionDescriptionString: String
+)
+
+data class IceMessage(val fromUser: String, val toUser: String, val iceCandidate: String)
